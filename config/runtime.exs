47d90e9f74a1bbff0ed29c0ed4010b52823de18f
dev-rroslan <dev.rroslan@gmail.com>
2022-05-27 00:56:08 +0800

    add check_origin urls

--- config/runtime.exs
+++ config/runtime.exs
@@ -53,6 +53,7 @@ if config_env() == :prod do
 
   config :survey_live, SurveyLiveWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: ["https://surveylive.online", "https://wispy-moon-7859.fly.dev/"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
